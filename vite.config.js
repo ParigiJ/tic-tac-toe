5d0d07dc5155185cf481a3a2b892d7dd76d58752
ParigiJ <135647142+ParigiJ@users.noreply.github.com>
2025-02-26 18:44:46 -0600

    Chore: Rebuilt project to include missing assets

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/tic-tac-toe/",
-  build: {
-    outDir: "dist", // Ensure build output is correct
-  },
   plugins: [react(), tailwindcss()],
 });
